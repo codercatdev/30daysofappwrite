2bb7f16937785c252ebc377e9d632f8e2e3362c3
Alex <alex@codingcat.dev>
2023-01-12 17:14:37 -0500

    removed tailwind as it interferes with aw-lab/ui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,16 @@
 <script>
-	import '../app.postcss';
 	import '@aw-labs/ui/dist/appwrite.css';
-	import TopNav from '$lib/components/TopNav.svelte';
+	import '@aw-labs/icons/dist/icon.css';
+	import TopNav from '$lib/components/navigation/TopNav.svelte';
+	import SideNav from '$lib/components/navigation/SideNav.svelte';
+	import Footer from '$lib/components/layout/Footer.svelte';
 </script>
 
 <main class="grid-with-side" id="main">
 	<TopNav />
+	<SideNav />
 	<div class="main-content" id="content">
 		<slot />
 	</div>
+	<Footer />
 </main>
