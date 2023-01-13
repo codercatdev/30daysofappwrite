e5fdc8dabf5bb8bf02a91a1adb817ff393fbed07
Alex <alex@codingcat.dev>
2023-01-13 15:42:22 -0500

    add footer links

--- src/lib/components/layout/Footer.svelte
+++ src/lib/components/layout/Footer.svelte
@@ -1,1 +1,12 @@
-<footer class="main-footer">Footers</footer>
+<footer class="main-footer">
+	<div class="flex justify-center gap-4 m-8">
+		<a class="is-secondary is-big" href="https://appwrite.io">
+			<span class="text">Appwrite.io</span>
+			<span class="icon-external-link" aria-hidden="true" />
+		</a>
+		<a class="is-secondary is-big" href="https://appwrite.io">
+			<span class="text">Docs</span>
+			<span class="icon-external-link" aria-hidden="true" />
+		</a>
+	</div>
+</footer>
