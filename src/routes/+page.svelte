21d059d07901a243984fca29d3c1646f485b79d9
Alex <alex@codingcat.dev>
2023-01-13 16:36:37 -0500

    add better homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,24 @@
-Welcome
+<section class="grid gap-4 grid-cols-[repeat(auto-fit,minmax(300px,1fr))] sm:gap-10 m-8">
+	<a href="/30daysofappwrite">
+		<div class="card">
+			<div class="flex flex-col items-center">
+				<h1>30 Days of Appwrite</h1>
+				<p>A 30 day introducion to understand Appwrite further.</p>
+				<div class="flex h-72 w-72">
+					<img src="/images/logo.989bc816.png" alt="Person riding rocket into clouds" />
+				</div>
+			</div>
+		</div>
+	</a>
+	<a href="/appwrite-101">
+		<div class="card">
+			<div class="flex flex-col items-center">
+				<h1>Appwrite 101</h1>
+				<p>Overview on different Appwrite Products</p>
+				<div class="flex h-72 w-72">
+					<img src="/images/lessons.png" alt="Person riding rocket into clouds" />
+				</div>
+			</div>
+		</div>
+	</a>
+</section>
