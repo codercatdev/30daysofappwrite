3d6940da3fbe2806939f85e95fd80a8932b31778
Alex <alex@codingcat.dev>
2023-01-12 20:10:39 -0500

    add title

--- src/routes/30daysofappwrite/[slug]/+page.svelte
+++ src/routes/30daysofappwrite/[slug]/+page.svelte
@@ -6,15 +6,22 @@
 	const { post, prev, next } = data;
 </script>
 
-<section class="flex justify-center">
-	<div class="flex flex-col gap-4 lg:gap-8">
-		{#if post?.cover_image}
-			<div>
-				<img src={post.cover_image} alt={post.title} />
+{#if !post}
+	How did you get here?
+{:else}
+	<section class="flex justify-center m-2">
+		<div class="flex flex-col gap-4 lg:gap-8">
+			<!-- TODO: Make lazyload and responsive -->
+			{#if post?.cover_image}
+				<div>
+					<img src={post.cover_image} alt={post.title} />
+				</div>
+			{/if}
+			<h1 class="heading-level-1 s-Z7Cde-PLUW5d">{post.title}</h1>
+
+			<div class="prose lg:prose-xl xl:prose-2xl">
+				<svelte:component this={data?.post?.body} />
 			</div>
-		{/if}
-		<div class="prose lg:prose-xl xl:prose-2xl">
-			<svelte:component this={data?.post?.body} />
 		</div>
-	</div>
-</section>
+	</section>
+{/if}
