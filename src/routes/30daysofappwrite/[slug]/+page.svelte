482c9e38a33eee7c6af07f2104d1f889509da0de
Alex <alex@codingcat.dev>
2023-01-12 20:05:20 -0500

    add pages with prev and next

---
+++ src/routes/30daysofappwrite/[slug]/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+	import 'prism-themes/themes/prism-shades-of-purple.min.css';
+
+	/** @type {import('./$types').PageData} */
+	export let data;
+	const { post, prev, next } = data;
+</script>
+
+<section class="flex justify-center">
+	<div class="flex flex-col gap-4 lg:gap-8">
+		{#if post?.cover_image}
+			<div>
+				<img src={post.cover_image} alt={post.title} />
+			</div>
+		{/if}
+		<div class="prose lg:prose-xl xl:prose-2xl">
+			<svelte:component this={data?.post?.body} />
+		</div>
+	</div>
+</section>
